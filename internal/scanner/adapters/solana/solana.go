57aee97f7d7186e9db2aba86ea402a94b9f7b847
Eugene <we.danced.a.lot@gmail.com>
2022-12-06 11:22:26 +0100

    add comments

--- internal/scanner/adapters/solana/solana.go
+++ internal/scanner/adapters/solana/solana.go
@@ -6,6 +6,22 @@ type SolanaAdapter struct{}
 
 func (a *SolanaAdapter) Scan(host string) error {
 	fmt.Println(host)
+	// TODO:
+	// err = s.getNodes()
+
+	// Check if this node is validator or RPC
+	// err = s.checkNodeType()
+
+	// TODO: nmap scanner (https://github.com/Ullaakut/nmap)
+
+	// TODO: asn scanner
+
+	// TODO: methods scanner
 
 	return nil
+}
+
+func (a *SolanaAdapter) getNodes() error {
+	// Get list of nodes from host (getClusterNodes)
+	// put these nodes to DB, so they can be scheduled by scheduler on next iteration
 }
